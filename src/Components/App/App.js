a3c37c1f30d78a57f96881d571db9a4d3c7358c1
Alyssa Lundgren <lundgrea@gmail.com>
2019-10-13 16:15:22 -0600

    Integrate DELETE and POST methods in apiCalls and App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import Main from "../Main/Main";
-import { fetchAllFolders, fetchAllPalettes, fetchAPalette, fetchAFolder, fetchAFoldersPalettes } from '../../apiCalls/apiCalls'
+import { fetchAllFolders, fetchAllPalettes, fetchAPalette, fetchAFolder, fetchAFoldersPalettes, deleteFolder, deletePalette, postNewPalette, postNewFolder, patchAPalette} from '../../apiCalls/apiCalls'
 import "./App.css";
 
 let array = [];
@@ -15,7 +15,9 @@ class App extends Component {
       palettes: [],
       paletteID: "",
       currentPalette: [],
-      isLoading: true
+      isLoading: true,
+      networkMessage: '',
+      error: ''
     };
   }
 
@@ -64,45 +66,88 @@ class App extends Component {
     this.setState({ currentPalette: newArray });
   };
 
-  getAllFolders = () => {
-    fetchAllFolders()
-    .then(folders => this.setState({ folders }))
-    .then(isLoading => this.setState({ isLoading: false }))
-    .catch(error => error);
-  }
-
   componentDidMount = () => {
     this.generateRandomColors();
     this.getAllFolders();
     // this.getAllPalettes();
     this.getAFolder(11);
     this.getAFoldersPalettes(11);
+    // this.deleteAPalette(30)
+    // this.deleteAFolder(9);
     // this.grabAPalette(16)
+    // this.addNewPalette('Jolly', array[0], array[1], array[2], array[3], array[4], 11)
+    // this.addNewFolder('Jambo Rambo')
   };
-
-  // getAPalette = paletteId => {
-  //   fetchAPalette(paletteId)
-  //   .then(currentPalette => this.setState({ currentPalette }))
-  //   .catch(error => error);
-  // }
-
-  // getAllPalettes = () => {
-  //   fetchAllPalettes()
-  //   .then(palettes => this.setState({ palettes }))
-  //   .catch(error => error);
-  // }
 
   getAFolder = folderId => {
     fetchAFolder(folderId)
     .then(folder => this.setState({ folderID: folder[0].id }))
-    .catch(error => error);
+    .catch(error => this.setState({error}))
+  }
+
+  getAllFolders = () => {
+    fetchAllFolders()
+    .then(folders => this.setState({ folders }))
+    .then(isLoading => this.setState({ isLoading: false }))
+    .catch(error => this.setState({error}))
+  }
+
+  getAPalette = paletteId => {
+    fetchAPalette(paletteId)
+    .then(currentPalette => this.setState({ currentPalette }))
+  .catch(error => this.setState({error}))
   }
 
   getAFoldersPalettes = folderId => {
     fetchAFoldersPalettes(folderId)
     .then(palettes => this.setState({ palettes }))
-    .catch(error => error);
+    .catch(error => this.setState({error}))
   }
+
+  getAllPalettes = () => {
+    fetchAllPalettes()
+    .then(palettes => this.setState({ palettes }))
+  .catch(error => this.setState({error}))
+  }
+
+  addNewPalette = (name, c1, c2, c3, c4, c5, folderId) => {
+    postNewPalette(name, c1, c2, c3, c4, c5, folderId)
+    .then(networkMessage => this.setState({networkMessage}))
+    .catch(error => this.setState({error}))
+  }
+
+  addNewFolder = folderName => {
+    postNewFolder(folderName)
+    .then(networkMessage => this.setState({networkMessage}))
+    .catch(error => this.setState({error}))
+  }
+
+  deleteAFolder = folderId => {
+    deleteFolder(folderId)
+    .then(networkMessage => this.setState({networkMessage}))
+    .catch(error => this.setState({error}))
+  }
+
+  deleteAPalette = paletteId => {
+    deletePalette(paletteId)
+    .then(networkMessage => this.setState({networkMessage}))
+    .catch(error => this.setState({error}))
+  }
+
+  updateAPalette = paletteId => {
+    patchAPalette(paletteId)
+   .then(networkMessage => this.setState({networkMessage}))
+    .catch(error => this.setState({error}))  
+  }
+
+  // updateAFolder = folderId => {
+  //   patchAFolder(folderId)
+  //   .then(networkMessage => this.setState({networkMessage}))
+  //   .catch(error => this.setState({error}))
+  // }
+
+
+ 
 
   render() {
     return (
--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -38,7 +38,6 @@ export const fetchAFolder = async folderId => {
   }
 }
 
-
 export const fetchAFoldersPalettes = async folderId => {
   try {
     const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/folders/${folderId}/palettes`)
@@ -48,3 +47,107 @@ export const fetchAFoldersPalettes = async folderId => {
     throw new Error(`Failed to get palettes with folder ID: ${folderId}`)
   }
 }
+
+export const deleteFolder = async folderId => {
+  try {
+    const option = {
+      method: "DELETE"
+    };
+    const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/folders/${folderId}`, option)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(`Could not delete folder with ID of: ${folderId} and its associated palettes`);
+  }
+};
+
+export const deletePalette = async paletteId => {
+  try {
+    const option = {
+      method: "DELETE"
+    };
+    const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/palettes/${paletteId}`, option)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(`Could not delete palette with ID: ${paletteId}`);
+  }
+};
+
+
+export const postNewPalette = async (name, c1, c2, c3, c4, c5, folderId) => {
+  const newPalette = 
+  { 
+    name: name, 
+    c1: c1, 
+    c2: c2, 
+    c3: c3, 
+    c4: c4, 
+    c5: c5, 
+    folder_id: folderId
+  }
+
+  try {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(newPalette),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/folders/${folderId}/palettes`, options)
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('There was a problem adding your palette');
+  }
+};
+
+export const postNewFolder = async name => {
+  const newFolder = { name: name }
+
+  try {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(newFolder),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/folders`, options)
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('There was a problem adding your folder');
+  }
+};
+
+
+export const patchAPalette= async (paletteId, key, newValue) => {
+  const updatedPalette = `{${key}: newValue }`
+
+  try {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(updatedPalette),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/palettes/${paletteId}`, options)
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('There was a problem updating your palette');
+  }
+};
+
+
+
+
+
+// updateAFolder = folderId => {
+//   updateFolder(folderId)
+//   .then(networkMessage => this.setState({networkMessage}))
+//   .catch(error => this.setState({error}))
+// }
