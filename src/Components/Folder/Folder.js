368083c3940c5fd0a095a675cdf9696d5ce34028
Ann Cerveny <cervann11@gmail.com>
2019-10-15 22:24:46 -0600

    Align Folder buttons

--- src/Components/Folder/Folder.js
+++ src/Components/Folder/Folder.js
@@ -4,9 +4,16 @@ import './Folder.css'
 const Folder = (props) => {
   return (
     <article className="Folder">
-      <h3 id="folder_name">{props.name}</h3>
-      <button onClick={(e) => props.getAFoldersPalettes(e, props.id)}>+</button>
-      <button>X</button>
+    {/* <img
+    id="open-folder-img"
+    src={require('../../assets/open-folder.svg')} 
+    onClick={(e) => props.getAFoldersPalettes(e, props.id)}/> */}
+    <button id="folder_name"
+    onClick={(e) => props.getAFoldersPalettes(e, props.id)}>{props.name}</button>
+    <img
+    id="delete-img"
+    src={require('../../assets/delete.svg')} 
+    />
     </article>
   )
 }
