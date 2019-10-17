157c930b8091f350e46bdabfb8af588010fcf54a
Ann Cerveny <cervann11@gmail.com>
2019-10-16 20:45:35 -0600

    Complete Colors Container test functionality

--- src/Components/ColorsContainer/ColorsContainer.test.js
+++ src/Components/ColorsContainer/ColorsContainer.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
+import Folder from '../../Components/Folder/Folder'
 import ColorsContainer from './ColorsContainer'
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -30,8 +31,12 @@ describe('ColorsContainer', () => {
   });
 
   it('should call deleteAPalette on a click', () => {
+    let palettes = [{name: "palette1", c1: "#FFFFFF", c2: "#FFFFFF", c3: "#FFFFFF", c4: "#FFFFFF", c5: "#FFFFFF", folder_id: 33}, {name: "palette2", c1: "#FFFFFF", c2: "#FFFFFF", c3: "#FFFFFF", c4: "#FFFFFF", c5: "#FFFFFF", folder_id: 33}]
+    const wrapperA = shallow(<Folder key="1" id="2" name={name} palettes={palettes} getAFoldersPalettes={jest.fn()} deleteAFolder={jest.fn()} onClick={jest.fn()}/>)
 
     const mockEvent = { preventDefault: jest.fn() }
+
+    wrapperA.find('#folder_name').simulate('click', mockEvent);
 
     const mockDeleteAPalette = jest.fn()
 
@@ -46,12 +51,13 @@ describe('ColorsContainer', () => {
       deleteAPalette={mockDeleteAPalette}
       folderID={16558}
       id={114}
-      palettesName={"Whale Life"}
+      paletteName={"Whale Life"}
+      onClick={jest.fn()}
       />);
 
-    wrapper.find('#delete-palette-img').simulate('click', mockEvent);
+    wrapper.find('img').simulate('click', mockEvent);
 
-    expect(mockDeleteAPalette).toHaveBeenCalled(mockEvent, 114, 16558);
+    expect(mockDeleteAPalette).toHaveBeenCalledWith(mockEvent, 114, 16558);
   });
 
 })
